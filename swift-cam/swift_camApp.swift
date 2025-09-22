20c53f4302253dc728f990d840de12394cf5bed2
pbarton <patrik.barton@bwsolution.de>
2025-09-22 16:20:06 +0200

    Initial Commit

---
+++ swift-cam/swift_camApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_camApp.swift
+//  swift-cam
+//
+//  Created by Patrik Barton on 22.09.25.
+//
+
+import SwiftUI
+
+@main
+struct swift_camApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
