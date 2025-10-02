c524376a182e2c107bde75a117b0ca11426c5da1
Joshua Noeldeke <joshua.noeldeke@me.com>
2025-10-02 12:03:03 +0200

    Added modelNotFound error case

--- swift-cam/Errors/ModelLoadingError.swift
+++ swift-cam/Errors/ModelLoadingError.swift
@@ -7,6 +7,22 @@
 
 import Foundation
 
-enum ModelLoadingError: Error {
+enum ModelLoadingError: LocalizedError {
+    case modelNotFound(String)
     case failedToLoad(String)
+    case neuralEngineFailure(String)
+    case cpuFallbackFailed(String)
+
+    var errorDescription: String? {
+        switch self {
+        case .modelNotFound(let modelName):
+            return "Model \(modelName) could not be found."
+        case .failedToLoad(let modelName):
+            return "Failed to load model \(modelName)."
+        case .neuralEngineFailure(let modelName):
+            return "Neural Engine failed to load \(modelName)."
+        case .cpuFallbackFailed(let modelName):
+            return "CPU fallback failed for \(modelName)."
+        }
+    }
 }
